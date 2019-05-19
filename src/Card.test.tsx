5865bab35b4f629235c92121f59f51fa014c6168
Brian Muthui <brian.muthui@andela.com>
2019-05-19 20:53:39 +0300

    Add Card component tests

---
+++ src/Card.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+import Card from './Card';
+
+
+describe('Card component', () => {
+  it('matches snapshot', () => {
+    const Component = create(<Card><p>Card</p></Card>);
+    expect(Component.JSON).toMatchSnapshot();
+  });
+});

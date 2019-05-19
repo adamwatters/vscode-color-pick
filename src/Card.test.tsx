a31635d582bdb579ae294a3b1d2bb058ad98561d
Brian Muthui <brian.muthui@andela.com>
2019-05-19 21:08:37 +0300

    Update snapshot and failing test

--- src/Card.test.tsx
+++ src/Card.test.tsx
@@ -6,6 +6,6 @@ import Card from './Card';
 describe('Card component', () => {
   it('matches snapshot', () => {
     const Component = create(<Card><p>Card</p></Card>);
-    expect(Component.JSON).toMatchSnapshot();
+    expect(Component.toJSON()).toMatchSnapshot();
   });
 });

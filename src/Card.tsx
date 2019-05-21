4d394c06f43635f042244277daf57a851663833d
Brian Muthui <brian.muthui@andela.com>
2019-05-21 15:44:58 +0300

    Use a function component for the Card component

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,35 +4,25 @@ interface ICardProps {
   children: {};
 }
 
-export default class Card extends React.Component {
-  public props: ICardProps;
-
-  constructor(props: ICardProps) {
-    super(props);
-    this.props = props;
-  }
-
-  public render() {
-    return (
-    <div
-      style={{
-        alignItems: "flex-start",
-        color: "black",
-        background: "white",
-        borderRadius: "4px",
-        boxShadow:
-          "rgba(0, 0, 0, 0.15) 0px 0px 0px 1px, rgba(0, 0, 0, 0.15) 0px 8px 16px",
-        boxSizing: "initial",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        marginTop: "16px",
-        padding: "10px",
-        width: "200px",
-        fontSize: "16px"
-      }}
-    >
-      {this.props.children}
-    </div>);
-  }
-}
+export default (props: ICardProps) => (
+  <div
+    style={{
+      alignItems: "flex-start",
+      color: "black",
+      background: "white",
+      borderRadius: "4px",
+      boxShadow:
+        "rgba(0, 0, 0, 0.15) 0px 0px 0px 1px, rgba(0, 0, 0, 0.15) 0px 8px 16px",
+      boxSizing: "initial",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      marginTop: "16px",
+      padding: "10px",
+      width: "200px",
+      fontSize: "16px"
+    }}
+  >
+    {props.children}
+  </div>
+);
